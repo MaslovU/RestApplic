740db43febfd4645bc4fcb60ac4e565a5dccd0ce
umaslov <dopchar@gmail.com>
2019-08-27 20:42:41 +0500

    minimal rest

---
+++ src/main/java/maslov/aptitos/AptitosApplication.java
@@ -0,0 +1,13 @@
+package maslov.aptitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptitosApplication.class, args);
+	}
+
+}
