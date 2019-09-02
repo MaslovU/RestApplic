443f758a250a16acecd2f01a422ec2a815135740
umaslov <dopchar@gmail.com>
2019-09-02 20:47:25 +0500

    minor

--- src/main/java/maslov/aptitos/repo/EmployeesRepo.java
+++ src/main/java/maslov/aptitos/repo/EmployeesRepo.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface EmployeesRepo extends JpaRepository<Employees, Long> {
-
-    List<Employees> findByName(String name);
     List<Employees> findByNameContaining(String name);
 }
