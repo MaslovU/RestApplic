a72322636fe1fc5979dd693f5f2f33508c6f6ddc
umaslov <dopchar@gmail.com>
2019-09-02 21:10:53 +0500

    Find By Division is added

---
+++ src/main/java/maslov/aptitos/controller/EmployeeFromDivisionController.java
@@ -0,0 +1,28 @@
+package maslov.aptitos.controller;
+
+import maslov.aptitos.domain.Employees;
+import maslov.aptitos.repo.EmployeesRepo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("employeeFromDiv")
+public class EmployeeFromDivisionController {
+
+    private final EmployeesRepo employeeRepo;
+
+    public EmployeeFromDivisionController(EmployeesRepo employeesRepo) {
+        this.employeeRepo = employeesRepo;
+    }
+
+    @GetMapping
+    List<Employees> findEmployeeFromDivision(@RequestParam String text)
+    {
+        return employeeRepo.findByDivision(text);
+    }
+}
--- src/main/java/maslov/aptitos/repo/EmployeesRepo.java
+++ src/main/java/maslov/aptitos/repo/EmployeesRepo.java
@@ -2,9 +2,13 @@ package maslov.aptitos.repo;
 
 import maslov.aptitos.domain.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployeesRepo extends JpaRepository<Employees, Long> {
     List<Employees> findByNameContaining(String name);
+    @Query("select e from Employees e where e.division.text =:text")
+    List<Employees> findByDivision(String text);
 }
