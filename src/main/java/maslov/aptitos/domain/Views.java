b323ff4917be5bfec56b4df9432760094f483e42
umaslov <dopchar@gmail.com>
2019-09-29 13:45:39 +0500

    minor change

--- src/main/java/maslov/aptitos/domain/Views.java
+++ src/main/java/maslov/aptitos/domain/Views.java
@@ -2,8 +2,7 @@ package maslov.aptitos.domain;
 
 public final class Views {
 
-    public interface Id {}
+    interface Id {}
     public interface IdName extends Id {}
     public interface FullMessage extends Id {}
-
 }
